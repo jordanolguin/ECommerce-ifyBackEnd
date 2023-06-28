9139951f5544eaa9e769f74f5f39042d4d415b33
Jordan Olguin <jordanolguin@iMac.local>
2023-06-28 09:59:08 -0600

    fixed bugs

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,8 +5,8 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
-    const tagData = await ProductTag.findAll({
-      include: [{ model: Product }, { model: Tag }],
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -16,8 +16,8 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const tagData = await ProductTag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: Tag }],
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
     });
     if (!tagData) {
       res.status(404).json({ message: "No tags found with that id!" });
